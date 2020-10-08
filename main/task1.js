b3813099cf4ba6bc2b2fe59195ac22aa5e4db751
Vladyslav Stetsenko <vladyslav.stetsenko@nexora.ch>
2020-10-08 23:11:26 +0200

    Added uploading photo coverage

--- main/task1.js
+++ main/task1.js
@@ -122,52 +122,33 @@ describe("Login", function () {
 
     await nxutils.waitUntilObjectIsVisibleAndClick("[id='nextStep']");
 
-    //4th page  (Uncommented solution for an element with input tag (input tag is required for an image to be added using the .sendKeys()))
+    //4th page  
     await browser.pause(2000);
 
-    const remoteFilePath = await browser.uploadFile(
-      "/Users/vladyslaw/Desktop/product/football2/main/sub-main/resources/operator_logo.png"
-    );
-    const paste = await browser.$('[id="upload"]');
-    await paste.scrollIntoView();
-    //await paste.doubleClick();
-    await paste.addValue(remoteFilePath);
-    //await paste.setValue(remoteFilePath);
-    //await paste.setValue(remoteFilePath);
-    await browser.pause(10000);
+    const venuePhoto = await browser.uploadFile(
+      "/Users/vladyslaw/Desktop/spaceShare/qa/c8b468ed42eb9036e1a0c5aa68e9.jpeg");
+    const dropBoxes = await browser.$$('[class="dz-hidden-input"]');
+    const dropZoneVenue = dropBoxes[1]
+    const dropZoneInteriorVenue = dropBoxes[0]
 
-    // var path = require("path");
-    // var toUpload = await path.join(__dirname, "..", "resources",
-    //     "operator_logo.png");
-    // await browser.uploadFile('[id="photoDropzone"]', toUpload);
+    await dropZoneVenue.addValue(venuePhoto);
+    await browser.pause(4000);
+    await dropZoneInteriorVenue.addValue(venuePhoto);
+    await browser.pause(5000);
+    await dropBoxes[2].addValue(venuePhoto);
+    await browser.pause(5000);
+    await dropZoneVenue.addValue(venuePhoto);
+    await browser.pause(5000);
+    await dropZoneVenue.addValue(venuePhoto);
+    await browser.pause(3000);
+    await dropZoneVenue.addValue(venuePhoto);
+    await browser.pause(3000);
 
-    //const path = require('path');
-    //const filePath = await path.join("/Users/vladyslaw/Desktop/product/football2/main/sub-main/resources/operator_logo.png");
-    // const fileUpload = await browser.$('[id="upload"]');
-    // await browser.execute(
-    //   // assign style to elem in the browser
-    //   el => (el.style.display = "block"),
-    //   // pass in element so we don't need to query it again in the browser
-    //   fileUpload
-    // );
-    // await fileUpload.waitForDisplayed();
+    await nxutils.waitUntilObjectIsVisibleAndClick("[id='nextStep']")
 
-
-
-    // const input = await browser.$('[id="insidePhotosUpload"]')
-    // const submitBtn = await browser.$('[id="nextStep"]')
-
-    // // store test file path
-    // const filePath = path.join(__dirname, '/resources/operator_logo.png');
-
-    // // use browser.uploadFile to upload the test file
-    // const remoteFilePath = await browser.uploadFile(filePath);
-
-    // // access the test page
-    // //browser.url('/upload');
-
-    // // set file path value in the input field
-    // await input.setValue(remoteFilePath);
-    // await submitBtn.click()
+    //5th page
+    await browser.pause(2000)
+    await nxutils.waitUntilObjectIsVisibleAndClick("[id='nextStep']")
+    await browser.pause(2000)
   });
 });
